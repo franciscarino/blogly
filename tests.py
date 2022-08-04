2df101561f38dcc1159f8e22c527f7099268d06c
Francis Carino <fgcarino@gmail.com>
2022-08-04 13:56:02 -0700

    test

--- tests.py
+++ tests.py
@@ -1,7 +1,7 @@
 from unittest import TestCase
 
 from app import app, db
-from models import DEFAULT_IMAGE_URL, User
+from models import DEFAULT_IMAGE_URL, User, Post
 
 # Let's configure our app to use a different database for tests
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql:///blogly_test"
@@ -28,6 +28,7 @@ class UserViewTestCase(TestCase):
         # As you add more models later in the exercise, you'll want to delete
         # all of their records before each test just as we're doing with the
         # User model below.
+        breakpoint()
         User.query.delete()
 
         self.client = app.test_client()
@@ -43,6 +44,8 @@ class UserViewTestCase(TestCase):
             last_name="test_last_two",
             img_url=None,
         )
+
+        breakpoint()
 
         db.session.add_all([test_user, second_user])
         db.session.commit()
@@ -93,3 +96,52 @@ class UserViewTestCase(TestCase):
             html = resp.get_data(as_text=True)
             self.assertEqual(resp.status_code, 200)
             self.assertIn('Spin', html)
+
+
+# class PostViewTestCase(TestCase):
+#     """Test views for posts."""
+
+#     def setUp(self):
+#         """Create test client, add sample data."""
+
+#         Post.query.delete()
+
+#         self.client = app.test_client()
+
+#         test_user = User(
+#             first_name="test_first",
+#             last_name="test_last",
+#             img_url=None,
+#         )
+
+#         test_post = Post(
+#             title = "test_title",
+#             content = "test_content",
+#             user_id = 1
+#         )
+
+#         test_post_two = Post(
+#             title = "test_title_two",
+#             content = "test_content_two",
+#             user_id = 1
+#         )
+
+#         db.session.add_all([test_user, test_post, test_post_two])
+#         db.session.commit()
+
+
+#         self.user_id = test_user.id
+#         self.post_id = test_post.id
+
+#     def tearDown(self):
+#         """Clean up any fouled transaction."""
+#         db.session.rollback()
+
+#     def test_list_posts(self):
+#         """Tests posts list."""
+#         with self.client as c:
+#             resp = c.get("/users/1")
+#             self.assertEqual(resp.status_code, 200)
+#             html = resp.get_data(as_text=True)
+#             self.assertIn("test_title", html)
+#             self.assertIn("test_title_two", html)
